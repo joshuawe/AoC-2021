8dde8b2cbbc4b553f053368eaff850f68dbcadc7
Joshua Wendland <joshua.wendland@tum.de>
2021-12-05 13:01:08 +0100

    changed folder names

--- create_folders.py
+++ create_folders.py
@@ -3,10 +3,10 @@
 
 import os
 
-for i in range(1,25):
-    name = "Day " + f"{i:02}"
+for i in range(2,25):
+    name = "Day_" + f"{i:02}"
     print(name)
     try:
         os.mkdir(name)
     except OSError as e:
-        print(e)
+        print(e)  
